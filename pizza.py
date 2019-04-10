4b307d6a3c536b5df5409e3c0e8b30317f3b3e17
onfuego <tomsolise@gmail.com>
2019-04-10 10:57:33 -0400

    Ejercicio Pizza

---
+++ pizza.py
@@ -0,0 +1,91 @@
+ingredientes = ["masa tradicional", "salsa de tomate", "queso"]
+
+opcion = 1
+
+while opcion < 6:
+    print("Gracias por ordenar con nosotros, que desea realizar?")
+    print("1. Consultar ingredientes de la pizza")
+    print("2. Cambiar tipo de masa")
+    print("3. Cambiar el tipo de salsa")
+    print("4. Agregar ingredientes")
+    print("5. Eliminar ingredientes")
+    print("6. Ordenar")
+
+    opcion  = int(input("Favor ingrese una opcion: "))
+
+    if opcion == 1:
+        print("Los ingredientes seleccionados son: ", ingredientes)
+        print()
+
+    if opcion ==2:
+    #1. Consultar si entre los ingredientes esta la masa tradicional
+        if "masa tradicional" in ingredientes:
+    #1.1 Si esta, consultar si la desea cambiar por masa delgada
+            print("Cambiar masa tradicional por masa delgada?")
+            print("Escriba 'S' para cambiar, o 'N' para conservar masa tradicional")
+            print()
+            cambiar = input().upper()
+    #1.1.1 Si la quiere cambiar, eliminar de la lista masa tradicional y agregar la masa delgada
+            if cambiar == "S":
+                ingredientes.remove("masa tradicional")
+                ingredientes.insert(0, "masa delgada")
+    #1.1.2 Si no la quiere cambiar, volver a mostrar el menu
+
+    #1.2 Si no esta, consultar si la lista tiene la masa delgada
+        elif "masa delgada" in ingredientes:
+    #1.2.1 Si esta, consultar si la desea cambiar por masa tradicional
+            print("Cambiar masa delgada por masa tradicional?")
+            print("Escriba 'S' para cambiar o 'N' para conservar masa delgada")
+            cambiar == input().upper()
+    #1.2.1.1 Si dice que si, eliminar de la lista la masa delgada y agregar la tradicional
+            if cambiar == "S":
+                ingredientes.remove("masa delgada")
+                ingredientes.insert(0, "masa tradicional")
+    #1.2.1.2 Si dice que no, volver al menu
+
+    #1.3 No tiene ningun tipo de masa (else)
+        else:
+            print("Su pizza no tiene masa")
+    #1.3.1 Consultar con un nuevo submenu que masa desea: 1 Tradicional 2. Delgada
+        print("Escriba 1 si desea masa tradicional, o 2 si desea masa delgada")
+        masa = input()
+
+        if masa == "1":
+            ingredientes.insert(0, "masa tradicional")
+        elif masa == "2":
+            ingredientes.insert(0, "masa delgada")
+
+    if opcion == 3:
+        print()
+
+    if opcion == 4:
+        print("Que ingrediente desea agregar? (Escriba una de las siguientes opciones)")
+        print("Tomate")
+        print("Aceitunas")
+        print("Queso")
+        print("Peperoni")
+        print("Pollo")
+        nuevo = input().lower()
+
+        while nuevo !="tomate" and nuevo !="aceitunas" and nuevo !="queso" and nuevo !="peperoni" and nuevo !="pollo":
+            print("Ingrese un ingrediente valido")
+            print("Tomate")
+            print("Aceitunas")
+            print("Queso")
+            print("Peperoni")
+            print("Pollo")
+            nuevo = input().lower()
+
+        ingredientes.append(nuevo)
+
+    if opcion == 5:
+        print("Ingredientes actuales: ", ingredientes)
+        print("Escriba el que se desea eliminar")
+        eliminar = input().lower()
+
+        while eliminar not in ingredientes:
+                print("Ingredientes actuales: ", ingredientes)
+                print("Escriba el que se desea eliminar")
+                eliminar = input().lower()
+
+        ingredientes.remove(eliminar)
