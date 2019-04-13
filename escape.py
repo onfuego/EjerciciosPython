516b798a11a890a5fb4d03062575a9d19cc96cde
Tomás Solís <tomsolise@MacBook-Pro-15.local>
2019-04-13 10:41:38 -0400

    Ejercicio de velocidad de escape

--- escape.py
+++ escape.py
@@ -7,8 +7,7 @@ from math import sqrt
 g = float(sys.argv[1])
 #Usuario debe ingresar el valor del radio como segundo parametro
 r = float(sys.argv[2])
-
-#Se calcula la velocidad según la formula indicada
+#Se calcula la velocidad con los datos obtenidos
 velocidad = sqrt(2*g*r)
-#Se imprime la velocidad calculada según la formula de la linea 12 
+#Se imprime la velocidad calculada 
 print("La velocidad es {}".format(velocidad))
